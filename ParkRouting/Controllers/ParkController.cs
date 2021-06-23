6386cbb2eaec9b4c6632fc134fd295abdb8c9708
Skyler Aschenbeck <Skyler.Aschenbeck@improving.com>
2021-06-23 08:20:56 -0500

    Initial commit

---
+++ ParkRouting/Controllers/ParkController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ParkRouting.Controllers
+{
+    public class ParkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
